28933d48fa7ad36165c6b8b5156649fd5a157ed6
paigauresh <paigauresh@gmail.com>
2024-06-29 04:08:29 +0530

    rediret issue solved
    webname chanegd for a few

---
+++ app/admin/firebase/page.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import React, { useState } from 'react';
+import '@css/firebase.css';
+import Link from 'next/link';
+
+const UserCrudPage = () => {
+
+    const handleInputChange = (e) => {
+        //CODE HERE
+    };
+
+    const handleSubmit = (e) => {
+        //CODE HERE
+    };
+
+    const handleEdit = (index) => {
+       //CODE HERE
+    };
+
+    const handleDelete = (index) => {
+        //CODE HERE
+    };
+
+    return (
+        <section className="crudpage">
+            <div className="titlebar">
+                <img alt='logo' src="/logo.jpg" className="logo" />
+                <a className="web-name">ERP</a>
+                <Link
+                    title="home"
+                    className="material-symbols-outlined home"
+                    href={'/admin'}
+                >
+                    home
+                </Link>
+            </div>
+            <div className="crud-container">
+                <h2>User Management</h2>
+                <form className="user-form" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        required
+                    />
+                    <button type="submit">Add user</button>
+                </form>
+                {/** Table of the users displayed here whre you can edit or delete their creds or users*/}
+            </div>
+        </section>
+    );
+};
+
+export default UserCrudPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,95 @@
 "use client";
+
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import "@css/comingsoon.css";
-import Link from "next/link";
-export default function Appraisal() {
-    //if user not logged in, redirect to login page
-    const { data: session } = useSession();
-    if (!session) {
-        redirect("/");
+import { redirect, useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useFormStatus } from "react-dom";
+import { isAdmin } from "@/lib/mongoose/functions";
+
+export default function Home() {
+  const { data: session } = useSession();
+  if(session) redirect("/main")
+
+  //states
+  const [formData, setFormData] = useState({ id: "", password: "" });
+  const [authStatus, setAuthStatus] = useState(null);
+  const [pending, setPending] = useState(false)
+  /* event handlers */
+  //login event handler
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setPending(true)
+    const { id, password } = formData;
+    if (!id || !password) return setAuthStatus("Fill all details!");
+    const res = await signIn("credentials", {
+      id,
+      password,
+      redirect: false,
+    });
+    if (res.status == 200) setAuthStatus(null);
+    else {
+      setAuthStatus("Id and Password doesn't match. Try again.");
     }
+    setPending(false)
+  };
+  // update formdata to state
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  //submit button component
+  function SubmitButton() {
+    const { pending } = useFormStatus();
     return (
-        <>
-            <div className="titlebar">
-                <img alt="logo" src="/logo.jpg" className="logo" />
-                <div className="information">
-                    <h2>AJ Institute of Medical Sciences and Research centre</h2>
-                    <h3>Employee Management System</h3>
-                </div>
-                <Link className="clogout" href="/main">
-                    Home
-                </Link>
-            </div>
-            <div className="comingsoon">
-                Feature Comming Soon!
-            </div>
-        </>
+      <input type="submit" value={pending?"Logging in...":"Login"} />
     );
+  }
+
+  return (
+    <>
+      <link rel="stylesheet" href="css/page.css?v=1.0.0" />
+      <div className="loginContainer">
+        <a className="head">Faculty login</a>
+        <hr />
+        <form onSubmit={handleLogin}>
+          <label htmlFor="id">
+            <b>ID</b>
+          </label>
+          <input
+            name="id"
+            type="text"
+            id="id"
+            placeholder="id"
+            value={formData.id}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+
+          <label htmlFor="password">
+            <b>Password</b>
+          </label>
+          <input
+            name="password"
+            type="password"
+            id="password"
+            placeholder="Enter Password"
+            autoComplete="off"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          {authStatus && (
+            <span style={{ color: "black", fontSize: "small" }}>
+              {authStatus}
+            </span>
+          )}
+          <input type="submit" value={pending?"Logging in..":"Login"} />
+          {/* <SubmitButton/> */}
+        </form>
+      </div>
+    </>
+  );
 }
